e277bf54102bbf911668d0613ee148184a358790
Matt Thompson <matthewbot@gmail.com>
2014-02-06 16:19:42 -0500

    Too many tweaks to count

--- init.el
+++ init.el
@@ -11,30 +11,55 @@
 (setq inhibit-startup-message t
       auto-save-default nil
       make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(delete 'Git vc-handled-backends)
+(column-number-mode 1)
+(global-hl-line-mode 1)
+(setq mouse-wheel-scroll-amount '(5))
+(setq mouse-wheel-progressive-speed nil)
+(global-set-key (kbd "C-^") 'join-line)
+
+;; Abbrev
+(quietly-read-abbrev-file)
 
 ;; Window numbering
 (require 'window-numbering)
 (window-numbering-mode t)
 
 ;; Ido
-(require 'ido)
-(setq ido-enable-flex-matching t
-      ido-everywhere t
-      ido-use-filename-at-point 'guess) 
-(ido-mode t)
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
 
 ;; Magit
 (require 'magit)
 (global-set-key (kbd "C-c g") 'magit-status)
 
+;; Projectile
+(projectile-global-mode)
+
+;; Prodigy
+(require 'prodigy)
+(prodigy-define-service
+  :name "Kube"
+  :command "~/Projects/Kube/local/test"
+  :cwd "~/Projects/Kube/local")
+
+;; ERC
+(setq erc-autojoin-channels-alist '(("freenode.net" "##cpp ##cpp-general #emacs")
+                                    ("mozilla.org" "#rust #rust-gamedev")))
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+
 ;; C/C++
 (setq c-default-style "linux"
       c-basic-offset 4)
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.icc\\'" . c++-mode))
 (add-hook 'c++-mode-hook
 	  (lambda()
-	    (define-key c++-mode-map (kbd "C-c c") 'recompile)
-	    (define-key c-mode-map (kbd "C-c c") 'recompile)))
+	    (define-key c-mode-base-map (kbd "C-c c") 'recompile)))
 
 ;; Zenburn
 (load-theme 'zenburn t)
